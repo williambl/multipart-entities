90db161be2dc04f6fd40e56e6f6fbdd9807a1f51
williambl <william@williambl.com>
2021-06-22 00:15:46 +0100

    Implement TACS loading support for multipart entities

---
+++ src/main/java/com/williambl/multipartentities/mixin/ThreadedAnvilChunkStorageMixin.java
@@ -0,0 +1,24 @@
+package com.williambl.multipartentities.mixin;
+
+import com.williambl.multipartentities.MultipartEntity;
+import com.williambl.multipartentities.PartEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.world.ThreadedAnvilChunkStorage;
+import net.minecraft.util.TypeFilter;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Mixin(ThreadedAnvilChunkStorage.class)
+public class ThreadedAnvilChunkStorageMixin {
+    @Redirect(method = "loadEntity", at=@At(value = "CONSTANT", args = "classValue=net/minecraft/entity/boss/dragon/EnderDragonPart", ordinal = 0))
+    private Class<?> multipartentities$changeLoadingPartClass(Object targetObject, Class<?> classValue) {
+        return PartEntity.class;
+    }
+}
