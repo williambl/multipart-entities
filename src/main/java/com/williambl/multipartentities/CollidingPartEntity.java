2e197cbd18d3b20afa5bd8582ac472e41396b787
williambl <william@williambl.com>
2021-06-22 11:37:04 +0100

    Make AbstractPartEntity more useful as CollidingPartEntity

--- src/main/java/com/williambl/multipartentities/CollidingPartEntity.java
+++ src/main/java/com/williambl/multipartentities/CollidingPartEntity.java
@@ -20,14 +20,21 @@
 package com.williambl.multipartentities;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityPose;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.Packet;
 
-public abstract class AbstractPartEntity<T extends Entity> extends Entity {
+public class CollidingPartEntity<T extends Entity> extends Entity implements PartEntity<T> {
     private final T parent;
+    private final EntityDimensions dimensions;
 
-    public AbstractPartEntity(T parent) {
+    public CollidingPartEntity(T parent, float width, float height) {
         super(parent.getType(), parent.world);
         this.parent = parent;
+        this.dimensions = EntityDimensions.changing(width, height);
+        this.calculateDimensions();
     }
 
     public T getParent() {
@@ -35,7 +42,39 @@ public abstract class AbstractPartEntity<T extends Entity> extends Entity {
     }
 
     @Override
+    protected void initDataTracker() {
+    }
+
+    @Override
+    protected void readCustomDataFromNbt(NbtCompound nbt) {
+    }
+
+    @Override
+    protected void writeCustomDataToNbt(NbtCompound nbt) {
+    }
+
+    @Override
+    public boolean collides() {
+        return true;
+    }
+
+    @Override
+    public boolean isPartOf(Entity entity) {
+        return this == entity || this.parent == entity;
+    }
+
+    @Override
     public Packet<?> createSpawnPacket() {
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public EntityDimensions getDimensions(EntityPose pose) {
+        return this.dimensions;
+    }
+
+    @Override
+    public boolean shouldSave() {
+        return false;
     }
 }
